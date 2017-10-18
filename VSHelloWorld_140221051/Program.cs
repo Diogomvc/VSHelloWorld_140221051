284e05c0f665b8d7f7eb296860a450496afe0f4f
Diogo <memphislzr@gmail.com>
2017-10-18 14:26:16 +0100

    Segundo comentário

--- VSHelloWorld_140221051/Program.cs
+++ VSHelloWorld_140221051/Program.cs
@@ -15,6 +15,7 @@ namespace VSHelloWorld_140221051
         public static void Main(string[] args)
         {
             // Comentário de teste
+            // Segundo comentário
             BuildWebHost(args).Run();
         }
 
